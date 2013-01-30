0763ec5ef82a66825a9ec303ac50062f2948b8a2
John Kang <vtssogari@gmail.com>
2013-01-30 17:26:41 -0500

    add new

---
+++ src/test/java/org/kun/solr/test/SolrCopyCrawlerTest.java
@@ -0,0 +1,14 @@
+package org.kun.solr.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolrCopyCrawlerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
